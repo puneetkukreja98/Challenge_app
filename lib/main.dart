daa5ecba270d0a00d693c2eec2a6e91512e7b4b2
puneetkukreja98 <puneetkukreja98@gmail.com>
2021-05-12 00:30:11 +0530

    learning flutter

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.red,
+                margin: EdgeInsets.only(top: 30.0, bottom: 30.0),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellowAccent,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.pink,
+                  ),
+                ],
+              ),
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.blue,
+                margin: EdgeInsets.only(top: 30.0, bottom: 30.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
